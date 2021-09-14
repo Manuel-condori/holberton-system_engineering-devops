2e0f89ea2b32437ff7f03940006c0cf51823383a
Manuel-condori <2828@holbertonschool.com>
2021-09-13 23:28:21 -0500

    first commit to file gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python scrip  for a given employee ID
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    u = argv[1]
+    user = requests.get("http://jsonplaceholder.typicode.com/users/" + u).json()
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos?userId=" + u).json()
+    done = requests.get("http://jsonplaceholder.typicode.com/todos?userId=" +
+                        u + "&completed=true").json()
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get("name"), len(done), len(todos)))
+    for task in done:
+        print("\t " + task.get("title"))
