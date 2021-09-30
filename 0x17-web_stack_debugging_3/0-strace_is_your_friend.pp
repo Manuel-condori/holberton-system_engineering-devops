77956891391bef7a688c0476c8d2e890ab627499
Manuel-condori <2828@holbertonschool.com>
2021-09-30 12:56:55 -0500

    add content task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # script fix-wordpress settings
 exec { 'fix-wordpress':
   command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/bin', '/usr/sbin',]
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
